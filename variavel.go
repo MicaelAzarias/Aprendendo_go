3f83f98946fc4ed364a9af34873f2ceb32256c03
MicaelAzarias <143201335+MicaelAzarias@users.noreply.github.com>
2023-11-05 22:58:21 -0300

    variavel.go

---
+++ variavel.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	nome := "Micael" // a variavel 'var' não é necessaria para fazer uma função
+	idade := 22      // tambem não é preciso por a forma que a minha variavel vai receber por exemplo
+	versao := 1.1    // não é necessario por 'string', 'int', "float", se não colocar o float o go vai sempre jogar ara o maior float que tem o 64
+	fmt.Println("ola, sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa esta na versão", versao)
+
+	fmt.Println("o tipo da variavel nome é", reflect.TypeOf(nome))
+	fmt.Println("o tipo da variavel iade é", reflect.TypeOf(idade))
+}
+
+// colocar ':=' ele identifica uma variavel
